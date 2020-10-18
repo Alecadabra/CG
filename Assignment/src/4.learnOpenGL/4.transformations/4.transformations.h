37b4f79323ea243fc930ecc5fbf2b386ddc670b8
Alec <alecadab@gmail.com>
2020-10-18 11:43:08 +0800

    Made separate shader and textures classes

---
+++ Assignment/src/4.learnOpenGL/4.transformations/4.transformations.h
@@ -0,0 +1,26 @@
+// Preprocessor Directives
+#ifndef HELLO_TRIANGLE_H
+#define HELLO_TRIANGLE_H
+#pragma once
+
+// System Headers
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+//#include <btBulletDynamicsCommon.h>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Util headers
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+// Reference: https://github.com/nothings/stb/blob/master/stb_image.h#L4
+// To use stb_image, add this in *one* C++ source file.
+//     #define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#endif 
